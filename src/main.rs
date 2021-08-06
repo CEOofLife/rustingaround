615a3542e7b78c4ccd8f366c7245f9ee724a2825
CEOofLife <CEOofLife@users.noreply.github.com>
2021-08-06 21:12:35 +0200

    first rusty exp

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,30 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    say();
+    println!("Guessing game!");
     let secret = rand::thread_rng().gen_range(1..101); 
+    // println!("the secret number is {}", secret);
+    loop {
+    println!("Put in your number!");
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line!");
     
     println!("Your number is {}", guess);
-    let guess: u32 = guess.trim().parse().expect("Please type a num!");
-    match guess.cmp(&secret){
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
+    
+    let guess: u32 = match guess.trim().parse(){
+        Ok(num) => num,
+        Err(_) => continue,
+    };
+        match guess.cmp(&secret){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
-
-
 }
-fn say() {
-    println!("Guessing game!");
-    println!("Put in your number!");
-       
-}
+
